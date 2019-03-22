8189936c81138fd7c674dde801c47b19e69901a7
Dmitry Gromov <rubyumbra@gmail.com>
2019-03-22 22:47:06 +0300

    MVVM progress for detail activity

--- app/src/main/java/ru/ifmo/ctddev/gromov/vkphotosx/view/DetailActivity.kt
+++ app/src/main/java/ru/ifmo/ctddev/gromov/vkphotosx/view/DetailActivity.kt
@@ -2,20 +2,21 @@ package ru.ifmo.ctddev.gromov.vkphotosx.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_detail.*
 import ru.ifmo.ctddev.gromov.vkphotosx.R
+import ru.ifmo.ctddev.gromov.vkphotosx.databinding.ActivityDetailBinding
 import ru.ifmo.ctddev.gromov.vkphotosx.viewmodel.DetailViewModel
 
 class DetailActivity : AppCompatActivity() {
-    private lateinit var vm: DetailViewModel
+    private lateinit var viewmodel: DetailViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
-        vm = ViewModelProviders.of(this, DetailViewModel.ModelFactory(intent.getParcelableExtra("POST")))
+        viewmodel = ViewModelProviders.of(this, DetailViewModel.ModelFactory(intent.getParcelableExtra("POST")))
             .get(DetailViewModel::class.java)
-        vm.loadImage(image)
-        fave_button.setOnClickListener { vm.update(true) }
-        unfave_button.setOnClickListener { vm.update(false) }
+        DataBindingUtil.setContentView<ActivityDetailBinding>(this, R.layout.activity_detail).apply { vm = viewmodel }
+        viewmodel.loadImage(image)
     }
 }
